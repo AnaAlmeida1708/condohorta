8f2fee7e6ba3f092847a8367c423fd3fba2a3995
AnaAlmeida1708 <anacarolinama@gmail.com>
2020-02-28 21:03:30 -0300

    projeto criado

---
+++ src/main/java/br/com/fiap/condohorta/CondohortaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.condohorta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondohortaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondohortaApplication.class, args);
+	}
+
+}
