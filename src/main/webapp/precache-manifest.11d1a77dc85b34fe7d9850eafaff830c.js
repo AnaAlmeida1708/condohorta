71b227b0e22752e91517f5c3db874ecbec6f785c
AnaAlmeida1708 <anacarolinama@gmail.com>
2020-03-04 00:03:40 -0300

    inclusao build front

---
+++ src/main/webapp/precache-manifest.11d1a77dc85b34fe7d9850eafaff830c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33be61c9a8a8b55439780fb8218ffe1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9f077a3cfa431a03a8",
+    "url": "/static/js/2.de29da12.chunk.js"
+  },
+  {
+    "revision": "4684d803a9160af291cd0c44ed494b70",
+    "url": "/static/js/2.de29da12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2012cc9a0c9792db9d",
+    "url": "/static/js/main.42872d05.chunk.js"
+  },
+  {
+    "revision": "57c8ba8ab44a9ac6f350",
+    "url": "/static/js/runtime-main.8c07457c.js"
+  },
+  {
+    "revision": "e790357c16300f71b8c954e8e50f4d0b",
+    "url": "/static/media/brand.e790357c.svg"
+  }
+]);
